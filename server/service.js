2d02346975d9cd7983bfac69f389cdced846becd
dandan <ai_li_ti@sina.com>
2018-05-15 20:32:17 +0800

    Update service.js
    添加菜单权限

--- server/service.js
+++ server/service.js
@@ -14,7 +14,7 @@ var router = function(app){
 		console.log(account,password);
 		// if(account == "vadmin" && password == "v123456"){
 		if(account == "v" && password == "v"){
-			res.send({"code":0,"msg":"ok"});
+			res.send({"code":0,"msg":"ok",data:{authority:["home","page01","page02"]});
 		}else{
 			res.send({"code":10001,"msg":"用户名密码错误"});
 		}
