90c3ed3b2f4c1334a597395fc7b715d4dcc23f8a
dandan <ai_li_ti@sina.com>
2018-07-05 15:35:36 +0800

    修改提示框

--- src/packages/msgAlert/alert.vue
+++ src/packages/msgAlert/alert.vue
@@ -1,8 +1,9 @@
 <template>
 	<div class="alert" if-show="showAlert">
-		<div class="alert-box">
-			this is a valert
-			<slot name="alertMain"></slot>			
+		<div class="alert-box" :class="alertType">
+			<slot name="alertMain">
+				<span class="msg"><i class="alert-icon fa" :class="iconName"></i>{{msgMain}}</span>
+			</slot>			
 		</div>
 	</div>
 </template>
@@ -15,11 +16,31 @@
 				type:Boolean,
 				require:false,
 				default:false
+			},
+			alertType:{
+				type:String,
+				require:false
+			},
+			msgMain:{
+				type:String,
+				require:true
 			}
 		},
 		data () {
 			return {
-
+				iconName:''
+			}
+		},
+		created:function(){
+			this.createIconName();
+		},
+		methods:{
+			createIconName: function(){
+				switch(this.alertType){
+					case 'warn'		: this.iconName = 'fa-warning'; 		break;
+					case 'success'  : this.iconName = 'fa-circle-o-notch' ; break;
+					case 'error' 	: this.iconName = ' fa-times-circle' ; 	break;
+				}
 			}
 		}
 	}
@@ -28,12 +49,39 @@
 <style>
 	.alert{
 		position: absolute;
-		left: 50%;
-		top:45%;
+		left: 0;
+		right: 0;
+		top:15%;
+		margin:0 auto;
+		z-index: 9999;
 	}
 	.alert-box{
-		width: 100px;
-		height:80px;
-		color:red;
+		display: inline-block;
+		padding: 15px;
+		color:#333;
+		border-radius: 2px;
+		max-width: 430px;
+	}
+	.alert-box .msg{
+		display: inline-block;
+		width: 100%;
+		line-height: 22px;
+		text-align: center;
+		font-size: 16px;
+	}
+	.alert-box.error{
+		background: #e89191;
+		color:#fff;
+	}
+	.alert-box.warn{
+		background: #f1ce7e;
+		color:#fff;
+	}
+	.alert-box.success{
+		background: #90cc8d;
+		color:#fff;
+	}
+	.alert-box .alert-icon{
+		margin-right: 5px;
 	}
 </style>
