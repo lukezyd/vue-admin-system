86fbe4f2977d1252b38f6fb2887d306ff9b55e35
爱莉缇 <ai_li_ti@sina.com>
2018-07-22 19:19:35 +0800

    提取样式到一个文件里

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,54 +1,63 @@
 <template>
-	<div class="home">
-		<div class="home-main">
-			<div class="data-sum">
-				<ul>
-					<li  v-for="(item,i) in sumList">
-						<span class="fa fl" :style="{background:colorList[i]}" :class="item.icon" ></span>
-						<div class="text-box">
-							<span class="sun-num">{{item.num}}</span>
-							<span class="sun-desc">{{item.desc}}</span>
-						</div>
-					</li>
-				</ul>
-			</div>
-			<div class="main-chart">
-				<zchart :chartId="chart01.chartId" :options="chart01.chartoptions" style="width:100%;height:100%"></zchart>
-			</div>
-			<div class="aside-chart">
-				<zchart :chartId="chart02.chartId" :options="chart02.chartoptions" style="width:100%;height:48%"></zchart>
-				<zchart :chartId="chart03.chartId" :options="chart03.chartoptions" style="width:100%;height:48%"></zchart>
-			</div>
-			<div class="new-user">
-				<div class="personal clearfix">
-					<span class="personal-logo fl"><img :src="personal.logo" alt=""></span>
-					<div>
-						<p class="personal-name">{{personal.name}}</p>
-						<p>{{personal.desc}}</p>
+	<div class="zHome clearfix">
+		<div class="zHome-datasum clearfix" >
+			<ul>
+				<li  v-for="(item,i) in sumList">
+					<span class="fa fl" :style="{background:colorList[i]}" :class="item.icon" ></span>
+					<div class="zHome-datasum__box">
+						<span class="sun-num">{{item.num}}</span>
+						<span class="sun-desc">{{item.desc}}</span>
 					</div>
-				</div>
-				<ul>
-					<li class="clearfix"  v-for="item in property">
-						<span class="fl"><i class="fa" :class="item.icon"></i></span>
-						<span>{{item.desc}}</span>
-						<span class="fr">{{item.num}}</span>
-					</li>
-				</ul>
-			</div>
-			<div class="tag-rank">
-				<div class="rank-top clearfix">
-					<h4 class="fl">排行榜</h4>
-				</div>
-				<ul>
-					<li v-for="(item,i) in tagList">
-						<span>{{i + 1}}</span>
-						<span>{{item.text}}</span>
-						<span>{{item.num}}</span>
-						<span>{{item.chart}}</span>
-					</li>
-				</ul>
-			</div>
+				</li>
+			</ul>
 		</div>
+		<div class="zHome-main__chart fl">
+			<zchart :chartId="chart01.chartId" :options="chart01.chartoptions" style="width:100%;height:100%"></zchart>
+		</div>
+		<div class="zHome-recommend fl">
+			<div class="zHome-recommend__info clearfix">
+				<span class="info-logo fl">
+						<img :src="personal.logo" alt="">
+				</span>
+				<div class="info-main clearfix">
+					<p class="info-main__name">{{personal.name}}</p>
+					<p>{{personal.desc}}</p>
+				</div>
+			</div>
+			<ul>
+				<li class="clearfix"  v-for="item in property">
+					<span class="fl"><i class="fa" :class="item.icon"></i></span>
+					<span>{{item.desc}}</span>
+					<span class="fr">{{item.num}}</span>
+				</li>
+			</ul>
+		</div>
+		<div class="zHome-rank fl">
+			<div class="zHome-rank__head clearfix">
+				<h4 class="fl">排行榜</h4>
+			</div>
+			<ul>
+				<li v-for="(item,i) in tagList">
+					<span>{{i + 1}}</span>
+					<span>{{item.text}}</span>
+					<span>{{item.num}}</span>
+					<span>{{item.chart}}</span>
+				</li>
+			</ul>
+		</div>
+		<div class="zHome-aside__chart fl">
+			<zchart :chartId="chart02.chartId" 
+					:options="chart02.chartoptions" 
+					:style="chart02Style"
+					style="width:100%;height:48%">
+			</zchart>
+			<zchart :chartId="chart03.chartId" 
+					:options="chart03.chartoptions" 
+					:style="chart03Style"
+					style="width:100%;height:48%">
+			</zchart>
+		</div>
+		
 	</div>
 </template>
 <script>
@@ -71,6 +80,9 @@
 				property:[
 					{icon:"fa-cubes",desc:"吃猫粮",num:12},
 					{icon:"fa-heartbeat",desc:"配玩耍",num:22},
+					{icon:"fa-hourglass-3",desc:"睡觉",num:15},
+					{icon:"fa-hourglass-3",desc:"睡觉",num:15},
+					{icon:"fa-hourglass-3",desc:"睡觉",num:15},
 					{icon:"fa-hourglass-3",desc:"睡觉",num:15}
 				],
 				lists:[
@@ -85,6 +97,9 @@
 					{text:"苹果",num:23,chart:""},
 					{text:"苹果",num:23,chart:""},
 					{text:"苹果",num:23,chart:""},
+					{text:"苹果",num:23,chart:""},
+					{text:"苹果",num:23,chart:""},
+					{text:"苹果",num:23,chart:""},
 					{text:"苹果",num:23,chart:""}
 				],
 				chart01:{
@@ -92,10 +107,10 @@
 					chartoptions:{
 			            color:["#bfc2cd"],
 			            xAxis: {
-			                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","袜子","袜子","衬衫","羊毛衫","羊毛衫"]
+			                data: ["布偶猫","折耳猫","英短猫","波斯猫","短毛猫","蓝猫","挪威猫","缅因猫","埃及猫","美短猫","孟买猫"]
 			            },
 			            series: [{
-			                name: '销量',
+			                name: '点击量',
 			                type: 'bar',
 			                data: [5, 20, 36, 10, 10, 20,10, 20,12,34,45]
 			            }]
@@ -134,6 +149,12 @@
 					    }]
 					}
 				},
+				chart02Style:{
+					'padding': '10px',
+					'background-color': '#d6aaaa',
+					'color':'#fff',
+					'border-radius': '5px'
+				},
 				chart03:{
 					chartId:"chart03",
 					chartoptions:{
@@ -167,6 +188,13 @@
 						    type: 'bar'
 						}]
 					}
+				},
+				chart03Style:{
+					'margin-top': '2%',
+					'padding': '10px',
+					'background-color': '#6cb2d9',
+					'color':'#fff',
+					'border-radius':' 5px'
 				}
 			}
 		},
@@ -174,135 +202,3 @@
 		}
 	}
 </script>
-
-<style>
-	.home{
-		height: 100%;
-		width: 100%;
-		position: relative;
-		background: #f1f2f7;
-	}
-	.home-main{
-		position: relative;
-		overflow: hidden;
-		background: #f1f2f7;
-		height: 100%;
-		padding: 20px;
-	}
-	.data-sum{
-		overflow: hidden;
-	}
-	.data-sum li{
-		list-style: none;
-		float: left;
-		width: 20%;
-		margin-right: 6.6666%;
-		text-align: center;
-		height: 80px;
-		background:#fff;
-		cursor: pointer;
-		border-radius: 5px;
-	}
-	.data-sum li:last-child{
-		margin-right: 0;
-	}
-	.data-sum li .fa{
-		padding:  24px;
-		color:#fff;
-		width: 40%;
-	}
-	.text-box{
-		text-align: center;
-		overflow: hidden;
-		padding: 10px 0;
-		border-left:none;
-	}
-	.sun-num{
-		font-size: 34px;
-		display: block;
-		font-weight: bold;
-	}
-	.sun-desc{
-		font-size: 13px;
-		color:#a2a2a2;
-	}
-	.main-chart{
-		margin: 25px 0;
-		width: 65%;
-		float: left;
-		height: 450px;
-		border-radius: 5px;
-	}
-	.aside-chart{
-		margin-top: 25px;
-		width: 33%;
-		height: 450px;
-		float: left;
-		margin-left: 2%;
-	}
-	#chart02{
-		padding: 10px;
-		background-color: #d6aaaa;
-		color:#fff;
-		border-radius: 5px;
-	}
-	#chart03{
-		margin-top: 2%;
-		padding: 10px;
-		background-color: #6cb2d9;
-		color:#fff;
-		border-radius: 5px;
-	}
-	.new-user{
-		clear: both;
-		width: 33%;
-		background: #fff;
-		float: left;
-	}
-	.personal{
-		padding: 15px;
-	}
-	.personal div{
-		overflow: hidden;
-	}
-	.personal p{
-		text-align: left;
-		padding-left: 20px;
-		margin-top: 12px;
-	}
-	.personal-name{
-		color:#4ac7ef;
-	}
-	.personal-logo img{
-		width: 120px;
-		height: 108px;
-	}
-	.new-user li{
-		list-style: none;
-		border-top: 1px solid #eee;
-		line-height: 45px;
-		text-align: center;
-		padding: 0 20px;
-	}
-	.tag-rank{
-		width: 65%;
-		margin-left: 1%;
-		height: 280px;
-		background: #fff;
-		float: right;
-	}
-	.rank-top{
-		padding:15px;
-	}
-	.tag-rank li{
-		list-style: none;
-		line-height: 42px;
-		padding:0 15px;
-		display: table;
-		width: 100%;
-		border-top:1px solid #eee;
-	}
-	.tag-rank li span{
-		display: table-cell;
-	}
-</style>
