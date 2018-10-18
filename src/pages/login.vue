1730908609524bf738996f4af15b28613c598d04
zhangyudan <zhangyudan@chainwaytsp.com>
2018-10-18 19:18:41 +0800

    login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,20 +1,26 @@
 <template>
 	<div class="login-bg">
 		<div class="login">
-			<div class="login-content">
-				<div class="login-item"><span class="login-logo">vue admin system</span></div>
-				<div class="login-item">
-					<el-input v-model="account" class="account" placeholder="用户名"></el-input>
+			<el-form :rules="rulesList" :model="loginForm" ref="loginForm">
+				<div class="login-content">
+					<div class="login-item"><span class="login-logo">vue admin system</span></div>
+					<el-form-item prop="account" class="login-item">
+						<el-input v-model="loginForm.account" class="account" placeholder="用户名"></el-input>
+					</el-form-item>
+					<div class="login-item">
+						<el-form-item prop="password" class="login-item">
+							<el-input v-model="loginForm.password" type="password" placeholder="密码"></el-input>
+						</el-form-item>
+					</div>
+					<el-form-item class="login-item remember-box">
+						<el-checkbox v-model="rememberMe">记住密码</el-checkbox>
+						<p class="login-item_error" v-show="loginError">{{errorText}}</p>
+					</el-form-item>
+					<el-form-item class="login-item">
+						<el-button type="primary" class="login-btn"  @click.native.prevent="login">登录</el-button>
+					</el-form-item>
 				</div>
-				<div class="login-item">
-					<el-input v-model="password" type="password" placeholder="密码"></el-input>
-				</div>
-				<div class="login-item remember-box">
-					<el-checkbox v-model="rememberMe">记住密码</el-checkbox>
-					<p class="login-item_error" v-show="loginError">{{errorText}}</p>
-				</div>
-				<div class="login-item"><input @click="login()" class="login-btn" type="button" value="登录"></div>
-			</div>
+			</el-form>
 		</div>	
 	</div>
 </template>
@@ -22,19 +28,41 @@
 	export default {
 		name: "login",
 		data: function(){
+			const validatorFn = function(rule, val, callback){
+				if(val.length < 6){
+					alert('不能少于六位');
+				}
+			}
 			return {
-				account:'',
-				password:'',
+				loginForm:{
+					account:'',
+					password:''
+				},
 				rememberMe:false,
 				errorText:'',
-				loginError:false
+				loginError:false,
+				rulesList:{
+					account:[{ required:true, message:'账号不能为空',trigger:'blur'}],
+					password:[{ required:true, message:'密码不能为空',trigger:'blur'}]
+				}
 			}
 		},
 		methods: {
 			login:function(){
 				var self = this;
-				var account = this.account;
-				var password = this.password;
+
+				this.$refs.loginForm.validate((valid) => {
+					console.log(valid);
+					if(valid){
+						alert("ok");
+					}else{
+						alert("error");
+						return;
+					}
+				})
+
+				var account = this.loginForm.account;
+				var password = this.loginForm.password;
 
 				this.$axios.post('/login',{
 					account:account,
@@ -70,26 +98,32 @@
 	}
 </script>
 <style>
-	.login-bg{width: 100%;height: 100%;overflow: hidden;background: url(/static/images/login.jpg) no-repeat;background-size: 100% 100%;}
-	
+	.login-bg{width: 100%;height: 100%;overflow: hidden;
+		background: url(/static/images/login.jpg) no-repeat;background-size: 100% 100%;
+	}
 	.login{
 		padding: 20px 0;
-		width: 100%;height:390px;background:rgba(80, 119, 125, 0.5);position: absolute;top: 30%;
+		width: 560px;
+		right:0;
+		height:420px;background:rgba(80, 119, 125, 0.5);position: absolute;top: 30%;
 	}
 	.login-content{
-		padding: 15px;
-		width: 500px;
+		padding: 15px 70px;
+		width: 450px;
 		height: 350px;
-		background: #fff;
+		background: #fdfdfd;
 		position: absolute;
+		left: 50%;
+		top:50%;
+		transform: translate(-50%,-50%);
 		right:200px;
+		border-radius: 3px;
 	}
 	.login-logo{
 		display: inline-block;
-		width: 60%;
 		height: 60px;
-		margin: 15px auto;
-		font-size: 34px;
+		margin: 10px auto;
+		font-size: 30px;
     	line-height: 60px;
     	color: #81b38a;
     	border-radius: 3px;
@@ -100,10 +134,14 @@
 		margin-bottom: 18px;
 	    font-size: 15px;
 	}
-	.login-item_error{color:red;display: inline-block;font-size: 13px;
-    margin-left: 20px;}
+	.login-item_error{
+		color:red;
+		display: inline-block;
+		font-size: 13px;
+   		margin-left: 20px;
+   	}
 	.login-item input{
-		width: 70%;
+		/*width: 70%;*/
 		height: 32px;
 		border:1px solid #ddd;
 		border-radius: 3px;
@@ -111,6 +149,7 @@
 		outline: none;
 	}
 	.login-item .login-btn{
+		width: 100%;
 		height: 36px;
 		background: #81b38a;
 	    color: #fff;
@@ -119,7 +158,7 @@
 	.login-btn:hover{
 		background: #5e9267;
 	}
-	.remember-box{text-align: left;margin-left: 15%;}
+	.remember-box{text-align: left;}
 	.el-checkbox__inner{width: 16px;height: 16px;}
 	.el-checkbox__inner::after{height: 8px;left: 5px;top:2px;}
 </style>
